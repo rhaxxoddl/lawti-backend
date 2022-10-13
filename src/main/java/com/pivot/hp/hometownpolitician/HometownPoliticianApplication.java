a422e10f9300992d0169d39d8f364984bb8610de
bigpel66 <bigpel66@icloud.com>
2022-10-13 15:41:38 +0900

    [Add] 프로젝트 설정

---
+++ src/main/java/com/pivot/hp/hometownpolitician/HometownPoliticianApplication.java
@@ -0,0 +1,13 @@
+package com.pivot.hp.hometownpolitician;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HometownPoliticianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HometownPoliticianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pivot/hp/hometownpolitician/HometownPoliticianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pivot.hp.hometownpolitician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HometownPoliticianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
