5bf79a50b47415c1402aa21b034acf57f3e62f78
bigpel66 <bigpel66@icloud.com>
2022-10-14 00:48:57 +0900

    [Add] AOP Proxy Enable

--- src/main/java/com/pivot/hp/hometownpolitician/HometownPoliticianApplication.java
+++ src/main/java/com/pivot/hp/hometownpolitician/HometownPoliticianApplication.java
@@ -2,12 +2,14 @@ package com.pivot.hp.hometownpolitician;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class HometownPoliticianApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HometownPoliticianApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HometownPoliticianApplication.class, args);
+    }
 
 }
