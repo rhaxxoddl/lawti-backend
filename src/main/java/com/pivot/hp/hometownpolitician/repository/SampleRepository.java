e56c845131ea03bbeaaecc48d787852bfe66a85e
bigpel66 <bigpel66@icloud.com>
2023-01-25 17:33:40 +0900

    [Change] 레포 리셋

--- src/main/java/com/pivot/hp/hometownpolitician/repository/SampleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.pivot.hp.hometownpolitician.repository;
-
-import com.pivot.hp.hometownpolitician.entity.Sample;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SampleRepository extends JpaRepository<Sample, Long> {
-}
-
