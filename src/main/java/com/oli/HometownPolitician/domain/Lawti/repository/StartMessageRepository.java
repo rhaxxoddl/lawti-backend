a5008443a0de030fb378b3b207b685db05dc1c3c
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-07 23:17:36 +0900

    [Add] StartMessage Repository 추가

---
+++ src/main/java/com/oli/HometownPolitician/domain/Lawti/repository/StartMessageRepository.java
@@ -0,0 +1,7 @@
+package com.oli.HometownPolitician.domain.Lawti.repository;
+
+import com.oli.HometownPolitician.domain.Lawti.entity.StartMessage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StartMessageRepository extends JpaRepository<StartMessage, Long> {
+}
