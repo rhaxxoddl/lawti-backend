c05cdde8b77d73c9affcde3d8ce3269df1c98b88
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-08 20:21:32 +0900

    [Change] StartMessagesDto from 파라미터 타입 변경
    List<StartMessageDto> -> List<StartMessage>

--- src/main/java/com/oli/HometownPolitician/domain/startMessage/service/StartMessageService.java
+++ src/main/java/com/oli/HometownPolitician/domain/startMessage/service/StartMessageService.java
@@ -1,14 +1,11 @@
 package com.oli.HometownPolitician.domain.startMessage.service;
 
-import com.oli.HometownPolitician.domain.startMessage.dto.StartMessageDto;
 import com.oli.HometownPolitician.domain.startMessage.dto.StartMessagesDto;
 import com.oli.HometownPolitician.domain.startMessage.repository.StartMessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,11 +14,7 @@ public class StartMessageService {
     private final StartMessageRepository startMessageRepository;
 
     public StartMessagesDto queryStartMessages() {
-        return StartMessagesDto.from(startMessageRepository
-                .findAll()
-                .stream()
-                .map(StartMessageDto::from)
-                .collect(Collectors.toList())
-        );
+        return StartMessagesDto
+                .from(startMessageRepository.findAll());
     }
 }
