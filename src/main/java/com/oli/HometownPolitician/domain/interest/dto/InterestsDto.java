eb3be706d61f6274b3ff017a182aa1adc03f6455
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-08 20:09:06 +0900

    [Change] InterestsDto from 파라미터 타입 변경
    List<InterestDto> -> List<Interest>

--- src/main/java/com/oli/HometownPolitician/domain/interest/dto/InterestsDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/interest/dto/InterestsDto.java
@@ -1,19 +1,25 @@
 package com.oli.HometownPolitician.domain.interest.dto;
 
+import com.oli.HometownPolitician.domain.interest.entity.Interest;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
 public class InterestsDto {
     private List<InterestDto> list;
 
-    static public InterestsDto from(List<InterestDto> interestDtoList) {
+    static public InterestsDto from(List<Interest> interestList) {
         return InterestsDto
                 .builder()
-                .list(interestDtoList)
+                .list(interestList
+                        .stream()
+                        .map(InterestDto::from)
+                        .collect(Collectors.toList())
+                )
                 .build();
     }
 
