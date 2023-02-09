e6775199906ca966ec7cdb241d573b8eb74ff9ba
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-09 22:23:43 +0900

    [Change] MVP에 맞게 User entity 변경

--- src/main/java/com/oli/HometownPolitician/domain/user/entity/User.java
+++ src/main/java/com/oli/HometownPolitician/domain/user/entity/User.java
@@ -1,66 +1,31 @@
 package com.oli.HometownPolitician.domain.user.entity;
 
-import com.oli.HometownPolitician.domain.user.enumeration.UserRole;
 import com.oli.HometownPolitician.global.entity.BaseTimeEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Entity
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
+@SequenceGenerator(name = "USER_SEQ_GENERATOR", sequenceName = "USER_SEQ")
 public class User extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "user_id", unique = true, nullable = false)
     private Long id;
 
-    @Column(name = "identifier", unique = true, nullable = false)
-    private String identifier;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-
-    @Column(name = "nickname", unique = true, nullable = false)
-    private String nickname;
-
-    @Column(name = "email", nullable = false)
-    private String email;
-
-    @Column(name = "provider", nullable = false)
-    private String provider;
-
-    @Column(name = "password_changed_at", nullable = false)
-    private LocalDateTime passwordChangedAt;
-
-    @Column(name = "phone")
-    private String phone;
-
-    @Column(name = "birth_date")
-    private LocalDate birthDate;
-
-    @Column(name = "message_token")
+    @Column(name = "message_token", unique = true)
     private String messageToken;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "role", nullable = false)
-    private UserRole role;
-
-    @Lob
-    @Column(name = "image_uri", nullable = false)
-    private String imageUri;
-
-    @PrePersist
-    public void prePersist() {
-        this.role = this.role == null ? UserRole.ROLE_USER : this.role;
-    }
+    @Column(name = "uuid", unique = true)
+    private String uuid;
 
 }
