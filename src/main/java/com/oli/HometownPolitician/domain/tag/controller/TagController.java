2dd93f889bcffa49b24f712e4bf91025d1ddb75a
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-10 16:06:48 +0900

    [Add] User UUID로 팔로우한 TagsDto 반환하는 Controller 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/tag/controller/TagController.java
+++ src/main/java/com/oli/HometownPolitician/domain/tag/controller/TagController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,8 +22,9 @@ public class TagController {
     }
 
     @QueryMapping(name = "queryFollowedTags")
-    public TagsDto queryFollowedTags() {
-        return null;
+    public TagsDto queryFollowedTags(@RequestHeader(name = "Authorization") String token)
+    {
+        return tagService.queryFollowedTagsByUserUuid(token);
     }
     @MutationMapping(name = "followMyTags")
     public void followMyTags(TagsInput tagsInput) {
