563d7572d7366a460c331cf421a6f4f67f6c6f3e
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-10 16:05:05 +0900

    [Add] User UUID로 팔로우한 Tag 반환하는 레포지토리 메서드 추가

---
+++ src/main/java/com/oli/HometownPolitician/domain/tag/repository/TagRepositoryImpl.java
@@ -0,0 +1,33 @@
+package com.oli.HometownPolitician.domain.tag.repository;
+
+import com.oli.HometownPolitician.domain.tag.entity.Tag;
+import com.oli.HometownPolitician.domain.user.repository.UserRepositoryCond;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import static com.oli.HometownPolitician.domain.tag.entity.QTag.tag;
+import static com.oli.HometownPolitician.domain.user.entity.QUser.user;
+import static com.oli.HometownPolitician.domain.entityRelation.entity.QUserTagRelation.userTagRelation;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+public class TagRepositoryImpl implements TagRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+    private final UserRepositoryCond userCond;
+
+    public TagRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+        this.userCond = new UserRepositoryCond();
+    }
+
+    @Override
+    public List<Tag> qFindFollowedTagsByUserUuid(String userUuid) {
+        return queryFactory
+                .selectFrom(tag)
+                .leftJoin(tag.followingUsers, userTagRelation)
+                .leftJoin(userTagRelation.user, user)
+                .where(
+                       userCond.userEqUuid(userUuid)
+                )
+                .fetch();
+    }
+}
