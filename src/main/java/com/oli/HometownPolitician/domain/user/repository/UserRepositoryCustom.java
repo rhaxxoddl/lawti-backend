72a3edcd938d939929828bad15c568e8fe5aa6fc
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-13 00:52:21 +0900

    [Add] User에 FollowedTags까지 조인해서 가져오는 레포지토리 함수 추가
    - 이전 커밋에서 추가 못한 패키지명 변경

--- src/main/java/com/oli/HometownPolitician/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/oli/HometownPolitician/domain/user/repository/UserRepositoryCustom.java
@@ -6,4 +6,5 @@ import java.util.Optional;
 
 public interface UserRepositoryCustom {
     Optional<User> qFindByUuid(String uuid);
+    Optional<User> qFindByUuidWithFollowedTags(String uuid);
 }
--- src/main/java/com/oli/HometownPolitician/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/oli/HometownPolitician/domain/user/repository/UserRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.oli.HometownPolitician.domain.user.repository;
 
 import static com.oli.HometownPolitician.domain.user.entity.QUser.user;
+import static com.oli.HometownPolitician.domain.tag.entity.QTag.tag;
+import static com.oli.HometownPolitician.domain.userTagRelation.entity.QUserTagRelation.userTagRelation;
 import com.oli.HometownPolitician.domain.user.entity.User;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -26,5 +28,17 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
                 )
                 .stream()
                 .findFirst();
+    }
+
+    @Override
+    public Optional<User> qFindByUuidWithFollowedTags(String uuid) {
+        return queryFactory
+                .selectFrom(user)
+                .where(
+                        userCond.userEqUuid(uuid)
+                                .and(userCond.userNotDeleted())
+                ).join(user.followedTags, userTagRelation)
+                .join(userTagRelation.tag, tag)
+                .stream().findFirst();
     }
 }
