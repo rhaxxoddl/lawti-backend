802b9f5906ebf537a668ceb024675f6d78fa05e6
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-13 20:53:57 +0900

    [Add] User entity에 followingTags, unfollowingTags 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/user/entity/User.java
+++ src/main/java/com/oli/HometownPolitician/domain/user/entity/User.java
@@ -1,13 +1,16 @@
 package com.oli.HometownPolitician.domain.user.entity;
 
+import com.oli.HometownPolitician.domain.tag.entity.Tag;
 import com.oli.HometownPolitician.domain.userTagRelation.entity.UserTagRelation;
 import com.oli.HometownPolitician.global.entity.BaseTimeEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -29,12 +32,34 @@ public class User extends BaseTimeEntity {
     @Column(name = "uuid", unique = true)
     private String uuid;
 
-    @OneToMany(mappedBy = "user")
-    private List<UserTagRelation> followedTags = new ArrayList<>();
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<UserTagRelation> followedTags = new HashSet<>();
 
     @Builder
     public User(String uuid) {
         this.uuid = uuid;
+    }
+
+    public void followingTags(List<Tag> tags) {
+        followedTags.addAll(tags
+                .stream()
+                .map(
+                        tag -> UserTagRelation
+                                .builder()
+                                .user(this)
+                                .tag(tag)
+                                .build()
+                )
+                .collect(Collectors.toList())
+        );
+    }
+
+    public void unfollowingTags(List<Long> tagIds) {
+        tagIds.forEach(tagId -> {
+            followedTags.removeIf(followedTag ->
+                    (followedTag.getTag().getId().equals(tagId))
+            );
+        });
     }
 
 }
