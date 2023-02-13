315acc3f9fd15f8f8276913c80427f227fbd22e1
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-13 20:38:52 +0900

    [Add] tag id 리스트로 리스트 안의 id에 해당하는 태그 찾아서 반환하는 레포지토리 함수 추가

--- src/main/java/com/oli/HometownPolitician/domain/tag/repository/TagRepositoryImpl.java
+++ src/main/java/com/oli/HometownPolitician/domain/tag/repository/TagRepositoryImpl.java
@@ -1,9 +1,13 @@
 package com.oli.HometownPolitician.domain.tag.repository;
 
+import com.oli.HometownPolitician.domain.tag.entity.Tag;
 import com.oli.HometownPolitician.domain.user.repository.UserRepositoryCond;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
 import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.oli.HometownPolitician.domain.tag.entity.QTag.tag;
 
 public class TagRepositoryImpl implements TagRepositoryCustom {
     private final JPAQueryFactory queryFactory;
@@ -12,5 +16,13 @@ public class TagRepositoryImpl implements TagRepositoryCustom {
     public TagRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
         this.userCond = new UserRepositoryCond();
+    }
+
+    @Override
+    public List<Tag> queryTagsByIds(List<Long> tagIds) {
+        return queryFactory
+                .selectFrom(tag)
+                .where(tag.id.in(tagIds))
+                .fetch();
     }
 }
