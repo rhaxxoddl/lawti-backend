63a1e5e13cf1778f661696dca659139b55ed85a3
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-18 20:45:43 +0900

    [Add] ProposersDto에 from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/dto/ProposersDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/dto/ProposersDto.java
@@ -1,12 +1,24 @@
 package com.oli.HometownPolitician.domain.bill.dto;
 
+import com.oli.HometownPolitician.domain.proposer.entity.Proposer;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Builder
 @Getter
 public class ProposersDto {
     private List<ProposerDto> proposers;
+
+    static public ProposersDto from(List<Proposer> proposerEntitys) {
+        return ProposersDto.builder()
+                .proposers(
+                        proposerEntitys
+                                .stream()
+                                .map(ProposerDto::from)
+                                .collect(Collectors.toList())
+                ).build();
+    }
 }
