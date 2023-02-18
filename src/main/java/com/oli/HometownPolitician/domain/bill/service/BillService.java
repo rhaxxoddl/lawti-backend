1da2165d25da1d9bf8c861fff0a7f8eb67405432
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-19 00:07:50 +0900

    [Add] BillService에  followBill 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/service/BillService.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/service/BillService.java
@@ -5,18 +5,24 @@ import com.oli.HometownPolitician.domain.bill.dto.BillPdfUriDto;
 import com.oli.HometownPolitician.domain.bill.dto.FollowingBillsDto;
 import com.oli.HometownPolitician.domain.bill.entity.Bill;
 import com.oli.HometownPolitician.domain.bill.input.BillInput;
+import com.oli.HometownPolitician.domain.bill.input.BillsInput;
 import com.oli.HometownPolitician.domain.bill.repository.BillRepository;
+import com.oli.HometownPolitician.domain.user.entity.User;
+import com.oli.HometownPolitician.domain.user.service.UserService;
 import com.oli.HometownPolitician.global.error.NotFoundError;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
 public class BillService {
     private final BillRepository billRepository;
+    private final UserService userService;
 
     public BillDetailDto queryBillDetail(BillInput billInput) {
         Bill bill = billRepository.findById(billInput.getBillId())
@@ -25,9 +31,19 @@ public class BillService {
     }
     public BillPdfUriDto queryBillPdfUri(BillInput billInput) {
         return null;
-    }public FollowingBillsDto followBill(BillInput billInput, String authorization) {
-        return null;
-    }public FollowingBillsDto unfollowBill(BillInput billInput, String authorization) {
+    }
+    public FollowingBillsDto followBill(BillsInput billsInput, String authorization) {
+        User user = userService.getUser(authorization);
+        List<Bill> bills = billRepository.queryBillsByIdList(
+                billsInput.getBillInputs()
+                        .stream()
+                        .map(BillInput::getBillId)
+                        .toList()
+        );
+        List<Bill> followBills = user.followBills(bills);
+        return FollowingBillsDto.from(followBills);
+    }
+    public FollowingBillsDto unfollowBill(BillsInput billsInput, String authorization) {
         return null;
     }
 }
