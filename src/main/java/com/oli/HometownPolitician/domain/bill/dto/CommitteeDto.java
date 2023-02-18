e78ffa6295bd08568dac45324e782e4793e64113
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-18 20:48:49 +0900

    [Add] CommitteeDto에 from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/dto/CommitteeDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/dto/CommitteeDto.java
@@ -1,5 +1,6 @@
 package com.oli.HometownPolitician.domain.bill.dto;
 
+import com.oli.HometownPolitician.domain.committe.entity.Committee;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -8,4 +9,11 @@ import lombok.Getter;
 public class CommitteeDto {
     private Long committeId;
     private String name;
+
+    static public CommitteeDto from(Committee committeeEntity) {
+        return CommitteeDto.builder()
+                .committeId(committeeEntity.getId())
+                .name(committeeEntity.getName())
+                .build();
+    }
 }
