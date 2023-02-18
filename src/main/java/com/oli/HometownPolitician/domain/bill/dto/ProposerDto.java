eb1c78488a11afb62639ae2c7b4875ca675f7588
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-18 20:36:42 +0900

    [Add] ProposerDto에 from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/dto/ProposerDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/dto/ProposerDto.java
@@ -1,5 +1,6 @@
 package com.oli.HometownPolitician.domain.bill.dto;
 
+import com.oli.HometownPolitician.domain.proposer.entity.Proposer;
 import com.oli.HometownPolitician.domain.proposer.enumeration.ProposerRole;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,4 +11,12 @@ public class ProposerDto {
     private Long politicianId;
     private String name;
     private ProposerRole role;
+
+    public ProposerDto from(Proposer proposer) {
+        return ProposerDto.builder()
+                .politicianId(proposer.getPolitician().getId())
+                .name(proposer.getPolitician().getName())
+                .role(proposer.getProposerRole())
+                .build();
+    }
 }
