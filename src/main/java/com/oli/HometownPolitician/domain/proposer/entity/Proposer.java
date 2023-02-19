740549c385d6a4528ca4d98b555f6363ddfa35cb
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-19 16:41:49 +0900

    [Add] Proposer from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/proposer/entity/Proposer.java
+++ src/main/java/com/oli/HometownPolitician/domain/proposer/entity/Proposer.java
@@ -3,16 +3,15 @@ package com.oli.HometownPolitician.domain.proposer.entity;
 import com.oli.HometownPolitician.domain.bill.entity.Bill;
 import com.oli.HometownPolitician.domain.politician.entity.Politician;
 import com.oli.HometownPolitician.domain.proposer.enumeration.ProposerRole;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "proposers")
@@ -31,4 +30,12 @@ public class Proposer {
     private Politician politician;
     @Enumerated(EnumType.STRING)
     private ProposerRole proposerRole;
+
+    static public Proposer from(Bill bill, Politician politician, ProposerRole proposerRole) {
+        return Proposer.builder()
+                .bill(bill)
+                .politician(politician)
+                .proposerRole(proposerRole)
+                .build();
+    }
 }
