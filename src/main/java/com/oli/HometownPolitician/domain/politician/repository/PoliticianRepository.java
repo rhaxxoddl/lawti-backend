12ea7e05cf5aeec58d1040c6fa2b2bf22da1dbb3
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-19 18:00:20 +0900

    [Add] PoliticianRepository 추가

---
+++ src/main/java/com/oli/HometownPolitician/domain/politician/repository/PoliticianRepository.java
@@ -0,0 +1,7 @@
+package com.oli.HometownPolitician.domain.politician.repository;
+
+import com.oli.HometownPolitician.domain.politician.entity.Politician;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PoliticianRepository extends JpaRepository<Politician, Long> {
+}
