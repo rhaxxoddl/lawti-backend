12a0a7b8e2a72457bcb9cf985da4c5f83e7e70d8
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-20 20:34:57 +0900

    [Add] Politician에 BaseTimeEntity 추가

--- src/main/java/com/oli/HometownPolitician/domain/politician/entity/Politician.java
+++ src/main/java/com/oli/HometownPolitician/domain/politician/entity/Politician.java
@@ -1,6 +1,7 @@
 package com.oli.HometownPolitician.domain.politician.entity;
 
 import com.oli.HometownPolitician.domain.proposer.entity.Proposer;
+import com.oli.HometownPolitician.global.entity.BaseTimeEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -16,7 +17,7 @@ import java.util.List;
 @Table(name = "politicians")
 @EntityListeners(AuditingEntityListener.class)
 @SequenceGenerator(name = "POLITICIAN_SEQ_GENERATOR", sequenceName = "POLITICIAN_SEQ")
-public class Politician {
+public class Politician extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "POLITICIAN_SEQ")
     @Column(name = "politician_id", unique = true, nullable = false)
