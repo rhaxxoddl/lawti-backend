7ae83cfe00ced36cee666475956c78c3065a54a6
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-20 17:02:25 +0900

    [Add] CommitteeRepository 추가

--- src/main/java/com/oli/HometownPolitician/domain/committe/repository/CommitteeRepository.java
+++ src/main/java/com/oli/HometownPolitician/domain/committe/repository/CommitteeRepository.java
@@ -1,5 +1,6 @@
-package com.oli.HometownPolitician.domain.committe.entity;
+package com.oli.HometownPolitician.domain.committe.repository;
 
+import com.oli.HometownPolitician.domain.committe.entity.Committee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommitteeRepository extends JpaRepository<Committee, Long> {
