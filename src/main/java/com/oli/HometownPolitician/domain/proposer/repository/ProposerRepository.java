a62eaa44ea9c5200a078d66c5a6c8ad4cce75e69
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-21 14:30:45 +0900

    [Add] ProposerRepositoryCustom, ProposerRepositoryImpl 추가

--- src/main/java/com/oli/HometownPolitician/domain/proposer/repository/ProposerRepository.java
+++ src/main/java/com/oli/HometownPolitician/domain/proposer/repository/ProposerRepository.java
@@ -3,5 +3,5 @@ package com.oli.HometownPolitician.domain.proposer.repository;
 import com.oli.HometownPolitician.domain.proposer.entity.Proposer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProposerRepository extends JpaRepository<Proposer, Long> {
+public interface ProposerRepository extends JpaRepository<Proposer, Long>, ProposerRepositoryCustom {
 }
