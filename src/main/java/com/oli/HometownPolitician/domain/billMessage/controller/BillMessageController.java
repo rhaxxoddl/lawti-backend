5e702fe82557def298371538bde3514ab2a69d90
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-21 20:44:57 +0900

    [Add] BillMessageController 추가, 메서드 프로토 타입만 정의

---
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/controller/BillMessageController.java
@@ -0,0 +1,34 @@
+package com.oli.HometownPolitician.domain.billMessage.controller;
+
+import com.oli.HometownPolitician.domain.billMessage.dto.BillMessageListDto;
+import com.oli.HometownPolitician.domain.billMessage.dto.BillMessageRoomListDto;
+import com.oli.HometownPolitician.domain.billMessage.input.BillMessageListInput;
+import com.oli.HometownPolitician.domain.billMessage.input.BillMessageRoomListInput;
+import com.oli.HometownPolitician.domain.billMessage.input.ExitBillMessageRoomListInput;
+import lombok.RequiredArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.ContextValue;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class BillMessageController {
+    @QueryMapping("queryBillMessageRoomList")
+    public BillMessageRoomListDto queryBillMessageRoomList(@Argument(name = "input") @Valid BillMessageRoomListInput billMessageRoomListInput, @ContextValue String authorization) {
+        return null;
+    }
+    @QueryMapping("queryBillMessageList")
+    public BillMessageListDto queryBillMessageList(@Argument(name = "input") @Valid BillMessageListInput billMessageListInput, @ContextValue String authorization) {
+        return null;
+    }
+    @MutationMapping("exitMessageRoom")
+    public BillMessageRoomListDto exitMessageRoom(@Argument(name = "input") @Valid ExitBillMessageRoomListInput exitBillMessageRoomListInput, @ContextValue String authorization) {
+        return null;
+    }
+}
