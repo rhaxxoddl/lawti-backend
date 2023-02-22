f82da12eb9472a6f81f4e6c304ada65df20e27d4
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-22 13:40:39 +0900

    [Add] billMatchTagByTagIdList cond 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepositoryCond.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepositoryCond.java
@@ -1,6 +1,9 @@
 package com.oli.HometownPolitician.domain.bill.repository;
 
+import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.BooleanExpression;
+
+import java.util.List;
 
 import static com.oli.HometownPolitician.domain.bill.entity.QBill.bill;
 
@@ -13,5 +16,9 @@ public class BillRepositoryCond {
 
     public BooleanExpression billNotDeleted() {
         return bill.deletedAt.isNull();
+    }
+
+    public Predicate billMatchTagByTagIdList(List<Long> tagIdList) {
+        return bill.tags.any().tag.id.in(tagIdList);
     }
 }
