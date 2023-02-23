931a58c2c9cd60169b16fd527020b09e7f905130
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-23 20:45:56 +0900

    [Add] queryBillMessageRoomList service 메서드 추가
    - numOfUnreadMessage type Long -> int

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/service/BillMessageService.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/service/BillMessageService.java
@@ -5,21 +5,31 @@ import com.oli.HometownPolitician.domain.billMessage.dto.BillMessageRoomListDto;
 import com.oli.HometownPolitician.domain.billMessage.input.BillMessageListInput;
 import com.oli.HometownPolitician.domain.billMessage.input.BillMessageRoomListInput;
 import com.oli.HometownPolitician.domain.billMessage.input.ExitBillMessageRoomListInput;
+import com.oli.HometownPolitician.domain.billMessage.repository.BillMessageRepository;
+import com.oli.HometownPolitician.domain.billUserRelation.entity.BillUserRelation;
 import com.oli.HometownPolitician.domain.billUserRelation.repository.BillUserRelationRepository;
+import com.oli.HometownPolitician.domain.user.equipment.UserPrefixEquipment;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
 public class BillMessageService {
     private final BillUserRelationRepository billUserRelationRepository;
+    private final BillMessageRepository billMessageRepository;
 
     public BillMessageRoomListDto queryBillMessageRoomList(BillMessageRoomListInput billMessageRoomListInput, String authorization) {
-//        List<BillUserRelation> billUserRelations = billUserRelationRepository
-        return null;
+        List<BillUserRelation> billUserRelations = billUserRelationRepository.qFindByUserUuidAndFilter(
+                billMessageRoomListInput,
+                UserPrefixEquipment.deletePrefix(authorization)
+        );
+
+        return BillMessageRoomListDto.from(billUserRelations, billMessageRoomListInput.getPagination());
     }
 
     public BillMessageListDto queryBillMessageList(BillMessageListInput billMessageListInput, String authorization) {
