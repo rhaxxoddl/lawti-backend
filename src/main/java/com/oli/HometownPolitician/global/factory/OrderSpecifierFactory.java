6a6653ead847f2a6bd4bc005b13b03efbf4371fb
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-23 09:56:51 +0900

    [Add] OrderSpecifierFactory 추가

---
+++ src/main/java/com/oli/HometownPolitician/global/factory/OrderSpecifierFactory.java
@@ -0,0 +1,40 @@
+package com.oli.HometownPolitician.global.factory;
+
+
+import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.PathBuilder;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class OrderSpecifierFactory {
+
+    static public OrderSpecifier<?> from(TargetSlicePaginationInput pagination, PathBuilder path, String property) {
+        if (pagination == null) {
+            return null;
+        }
+        return from(pagination.getIsAscending(), path, property);
+    }
+
+    static public OrderSpecifier<?> from(Boolean isAscending, PathBuilder path, String property) {
+        return new OrderSpecifier<>(
+                isAscending ? Order.ASC : Order.DESC,
+                getExpression(path, property)
+        );
+    }
+
+    static private PathBuilder getExpression(PathBuilder<?> path, String property) {
+        switch (property) {
+            case "id":
+                return path.get(property, Long.class);
+            case "createdAt":
+                return path.get(property, LocalDateTime.class);
+            case "proposeDate":
+                return path.get(property, LocalDate.class);
+        }
+        return null;
+    }
+
+}
