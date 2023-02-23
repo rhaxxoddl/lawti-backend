eb909d4aee91b5e0bd38614eb1710ca76add096c
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-23 21:57:30 +0900

    [Add] BillMessageRepositoryImpl 추가

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/repository/BillMessageRepositoryImpl.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/repository/BillMessageRepositoryImpl.java
@@ -1,13 +1,44 @@
 package com.oli.HometownPolitician.domain.billMessage.repository;
 
-import com.oli.HometownPolitician.domain.bill.entity.Bill;
-import com.oli.HometownPolitician.domain.bill.repository.BillRepositoryCustom;
+import static com.oli.HometownPolitician.domain.bill.entity.QBill.bill;
+import com.oli.HometownPolitician.domain.bill.repository.BillRepositoryCond;
+import com.oli.HometownPolitician.domain.billMessage.entity.BillMessage;
 
+import static com.oli.HometownPolitician.domain.billMessage.entity.QBillMessage.billMessage;
+
+import com.oli.HometownPolitician.domain.billMessage.input.BillMessageListInput;
+import com.oli.HometownPolitician.global.factory.OrderSpecifierFactory;
+import com.querydsl.core.types.dsl.PathBuilder;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import javax.persistence.EntityManager;
 import java.util.List;
 
-public class BillMessageRepositoryImpl implements BillRepositoryCustom {
+public class BillMessageRepositoryImpl implements BillMessageRepositoryCustom {
+    private final JPAQueryFactory queryFactory;
+    private final BillMessageRepositoryCond billMessageCond;
+    private final BillRepositoryCond billCond;
+
+    public BillMessageRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+        this.billMessageCond = new BillMessageRepositoryCond();
+        this.billCond = new BillRepositoryCond();
+    }
+
     @Override
-    public List<Bill> queryBillsByIdList(List<Long> idList) {
-        return null;
+    public List<BillMessage> qFindByBillId(BillMessageListInput billMessageListInput) {
+        return queryFactory.selectFrom(billMessage)
+                .join(billMessage.bill, bill).fetchJoin()
+                .where(
+                        billMessageCond.notDeleted()
+                                .and(billCond.billEqId(billMessageListInput.getBillId()))
+                                .and(billCond.billNotDeleted())
+                )
+                .orderBy(
+                        OrderSpecifierFactory.from(billMessageListInput.getPagination(), new PathBuilder(BillMessage.class, "billMessage"), "createdAt"),
+                        OrderSpecifierFactory.from(billMessageListInput.getPagination(), new PathBuilder(BillMessage.class, "billMessage"), "id")
+                )
+                .limit(billMessageCond.billMessageLimit(billMessageListInput.getPagination()))
+                .fetch();
     }
 }
