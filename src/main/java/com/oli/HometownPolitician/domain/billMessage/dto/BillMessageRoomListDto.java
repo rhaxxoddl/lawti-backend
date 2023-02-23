fe88dd5e491b22e70d9ca68ce7bc6e2788c0f8c0
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-23 20:39:12 +0900

    [Add] BillMessageRoomListDto from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/dto/BillMessageRoomListDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/dto/BillMessageRoomListDto.java
@@ -1,5 +1,7 @@
 package com.oli.HometownPolitician.domain.billMessage.dto;
 
+import com.oli.HometownPolitician.domain.billUserRelation.entity.BillUserRelation;
+import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
 import com.oli.HometownPolitician.global.argument.output.SlicePaginationDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,4 +13,15 @@ import java.util.List;
 public class BillMessageRoomListDto {
     private List<BillMessageRoomDto> list;
     private SlicePaginationDto pagination;
+
+    static public BillMessageRoomListDto from(List<BillUserRelation> billUserRelations, TargetSlicePaginationInput paginationInput) {
+        return BillMessageRoomListDto.builder()
+                .list(
+                        billUserRelations.stream()
+                                .map(BillMessageRoomDto::from)
+                                .toList()
+                )
+                .pagination(SlicePaginationDto.from(paginationInput, billUserRelations.size()))
+                .build();
+    }
 }
