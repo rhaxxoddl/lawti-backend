cf1c391f6b2bd678174a8d067c75c5744b23ee35
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-23 21:57:53 +0900

    [Add] BillMessageRepository에 BillMessageRepositoryCustom 연결

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/repository/BillMessageRepository.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/repository/BillMessageRepository.java
@@ -3,5 +3,5 @@ package com.oli.HometownPolitician.domain.billMessage.repository;
 import com.oli.HometownPolitician.domain.billMessage.entity.BillMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BillMessageRepository extends JpaRepository<BillMessage, Long> {
+public interface BillMessageRepository extends JpaRepository<BillMessage, Long>, BillMessageRepositoryCustom {
 }
