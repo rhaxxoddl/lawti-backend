0f87b1a5a9abc2ab8b637a2a8cecae6d787b9205
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-24 14:49:02 +0900

    [Add] MessageDto from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/dto/MessageDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/dto/MessageDto.java
@@ -1,5 +1,6 @@
 package com.oli.HometownPolitician.domain.billMessage.dto;
 
+import com.oli.HometownPolitician.domain.billMessage.entity.BillMessage;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,4 +10,12 @@ public class MessageDto {
     private Long messageId;
     private String messageContent;
     private boolean isRead;
+
+    static public MessageDto from(BillMessage billMessage, boolean isRead) {
+        return MessageDto.builder()
+                .messageId(billMessage.getId())
+                .messageContent(billMessage.getContent())
+                .isRead(isRead)
+                .build();
+    }
 }
