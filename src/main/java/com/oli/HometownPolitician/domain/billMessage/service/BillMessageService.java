8fbd9d40b8dfee674a3f573b8900485cd556197e
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-24 15:53:23 +0900

    [Add] exitMessageRooms service 메서드 구현

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/service/BillMessageService.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/service/BillMessageService.java
@@ -1,7 +1,11 @@
 package com.oli.HometownPolitician.domain.billMessage.service;
 
+import com.oli.HometownPolitician.domain.bill.entity.Bill;
+import com.oli.HometownPolitician.domain.bill.input.BillInput;
+import com.oli.HometownPolitician.domain.bill.repository.BillRepository;
 import com.oli.HometownPolitician.domain.billMessage.dto.BillMessageListDto;
 import com.oli.HometownPolitician.domain.billMessage.dto.BillMessageRoomListDto;
+import com.oli.HometownPolitician.domain.billMessage.dto.ExitMessageRoomResultDto;
 import com.oli.HometownPolitician.domain.billMessage.entity.BillMessage;
 import com.oli.HometownPolitician.domain.billMessage.input.BillMessageListInput;
 import com.oli.HometownPolitician.domain.billMessage.input.BillMessageRoomListInput;
@@ -9,7 +13,10 @@ import com.oli.HometownPolitician.domain.billMessage.input.ExitBillMessageRoomLi
 import com.oli.HometownPolitician.domain.billMessage.repository.BillMessageRepository;
 import com.oli.HometownPolitician.domain.billUserRelation.entity.BillUserRelation;
 import com.oli.HometownPolitician.domain.billUserRelation.repository.BillUserRelationRepository;
+import com.oli.HometownPolitician.domain.user.entity.User;
 import com.oli.HometownPolitician.domain.user.equipment.UserPrefixEquipment;
+import com.oli.HometownPolitician.domain.user.repository.UserRepository;
+import com.oli.HometownPolitician.global.error.NotFoundError;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -23,6 +30,8 @@ import java.util.List;
 public class BillMessageService {
     private final BillUserRelationRepository billUserRelationRepository;
     private final BillMessageRepository billMessageRepository;
+    private final UserRepository userRepository;
+    private final BillRepository billRepository;
 
     public BillMessageRoomListDto queryBillMessageRoomList(BillMessageRoomListInput billMessageRoomListInput, String authorization) {
         List<BillUserRelation> billUserRelations = billUserRelationRepository.qFindByUserUuidAndFilter(
@@ -35,10 +44,19 @@ public class BillMessageService {
 
     public BillMessageListDto queryBillMessageList(BillMessageListInput billMessageListInput) {
         List<BillMessage> billMessageList = billMessageRepository.qFindByBillId(billMessageListInput);
+
         return BillMessageListDto.from(billMessageList,billMessageListInput.getPagination());
     }
 
-    public BillMessageRoomListDto exitMessageRoom(ExitBillMessageRoomListInput exitBillMessageRoomListInput, String authorization) {
-        return null;
+    public ExitMessageRoomResultDto exitMessageRooms(ExitBillMessageRoomListInput exitBillMessageRoomListInput, String authorization) {
+        User user = userRepository.qFindByUuid(UserPrefixEquipment.deletePrefix(authorization)).orElseThrow(() -> new NotFoundError("해당 유저를 찾을 수 없습니다"));
+        List<Long> unfollowBillIds = exitBillMessageRoomListInput.getList().stream().map(BillInput::getBillId).toList();
+        List<Bill> unfollowBills = billRepository.queryBillsByIdList(unfollowBillIds);
+        user.unfollowBills(unfollowBills);
+
+        return ExitMessageRoomResultDto
+                .builder()
+                .isSuccess(true)
+                .build();
     }
 }
