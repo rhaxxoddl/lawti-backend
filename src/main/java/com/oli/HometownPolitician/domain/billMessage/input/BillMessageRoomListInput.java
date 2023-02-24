53e41cbd99d7990aa38a5e23df987380ea316187
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-24 17:29:10 +0900

    [Modify] BillMessageRoomListInput에서 from 메서드 제거

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/input/BillMessageRoomListInput.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/input/BillMessageRoomListInput.java
@@ -14,13 +14,4 @@ public class BillMessageRoomListInput {
     private BillMessageRoomFilterInput filter;
     @NotNull(message = "메세지 방들을 읽어오기 위한 값들이 입력되지 않았습니다")
     private TargetSlicePaginationInput pagination;
-
-    static public BillMessageRoomListInput from(Long target, Integer elementSize, Boolean isAscending) {
-        return BillMessageRoomListInput.builder()
-                .pagination(
-                        TargetSlicePaginationInput
-                                .from(target, elementSize, isAscending)
-                )
-                .build();
-    }
 }
