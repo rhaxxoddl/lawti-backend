808c92b7f2745fa1ba531b73c9f441c973e98129
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-24 20:57:42 +0900

    [Add] BillUserRelationCond에 target부터 가져오는 cond 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/billUserRelation/repository/BillUserRelationRepositoryCond.java
+++ src/main/java/com/oli/HometownPolitician/domain/billUserRelation/repository/BillUserRelationRepositoryCond.java
@@ -1,6 +1,7 @@
 package com.oli.HometownPolitician.domain.billUserRelation.repository;
 
 import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 import static com.oli.HometownPolitician.domain.billUserRelation.entity.QBillUserRelation.billUserRelation;
@@ -19,6 +20,17 @@ public class BillUserRelationRepositoryCond {
             return null;
         }
         return Long.valueOf(pagination.getElementSize());
+    }
+
+    public BooleanBuilder billUserRelationDirection(TargetSlicePaginationInput pagination) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (pagination == null || pagination.getTarget() == null) {
+            return builder.and(null);
+        } else if (pagination.getIsAscending()) {
+            return builder.and(billUserRelation.id.gt(pagination.getTarget()));
+        } else {
+            return builder.and(billUserRelation.id.lt(pagination.getTarget()));
+        }
     }
 
 }
