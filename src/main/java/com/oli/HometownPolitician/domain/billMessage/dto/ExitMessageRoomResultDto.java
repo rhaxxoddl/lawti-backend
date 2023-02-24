24d0e112812ca60e506b875438f4b02dbdb57c90
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-24 15:48:58 +0900

    [Modify] exitMessageRoom 반환값 타입 변경

---
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/dto/ExitMessageRoomResultDto.java
@@ -0,0 +1,10 @@
+package com.oli.HometownPolitician.domain.billMessage.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ExitMessageRoomResultDto {
+    boolean isSuccess;
+}
