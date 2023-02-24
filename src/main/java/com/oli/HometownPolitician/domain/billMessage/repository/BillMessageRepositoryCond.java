cde4b46f3da8a4535269ddb46f7b40ddd9542f58
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-24 20:53:56 +0900

    [Add] BillMessageCond에 target부터 가져오는 cond 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/billMessage/repository/BillMessageRepositoryCond.java
+++ src/main/java/com/oli/HometownPolitician/domain/billMessage/repository/BillMessageRepositoryCond.java
@@ -1,6 +1,7 @@
 package com.oli.HometownPolitician.domain.billMessage.repository;
 
 import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
 import static com.oli.HometownPolitician.domain.billMessage.entity.QBillMessage.billMessage;
@@ -15,5 +16,16 @@ public class BillMessageRepositoryCond {
             return null;
         }
         return Long.valueOf(pagination.getElementSize());
+    }
+
+    public BooleanBuilder billMessageDirection(TargetSlicePaginationInput pagination) {
+        BooleanBuilder builder = new BooleanBuilder();
+        if (pagination == null || pagination.getTarget() == null) {
+            return builder.and(null);
+        } else if (pagination.getIsAscending()) {
+            return builder.and(billMessage.id.gt(pagination.getTarget()));
+        } else {
+            return builder.and(billMessage.id.lt(pagination.getTarget()));
+        }
     }
 }
