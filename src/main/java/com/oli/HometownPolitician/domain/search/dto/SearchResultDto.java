5324963cca235c33016571da889fe4d5a2d1daa7
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 16:50:52 +0900

    [Add] SearchResultDto에 from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/search/dto/SearchResultDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/search/dto/SearchResultDto.java
@@ -2,6 +2,7 @@ package com.oli.HometownPolitician.domain.search.dto;
 
 import com.oli.HometownPolitician.domain.bill.dto.CommitteeDto;
 import com.oli.HometownPolitician.domain.bill.dto.ProposersDto;
+import com.oli.HometownPolitician.domain.bill.entity.Bill;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,4 +12,13 @@ public class SearchResultDto {
     private String title;
     private CommitteeDto committee;
     private ProposersDto proposers;
+
+    static public SearchResultDto from(Bill bill) {
+        return bill == null ? null : SearchResultDto
+                .builder()
+                .title(bill.getTitle())
+                .committee(CommitteeDto.from(bill.getCommittee()))
+                .proposers(ProposersDto.from(bill.getProposers()))
+                .build();
+    }
 }
