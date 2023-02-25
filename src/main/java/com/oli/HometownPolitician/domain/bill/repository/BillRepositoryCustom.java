380f80c77d715081f43a0eb4b2e137b65ad031fd
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 15:24:18 +0900

    [Add] BillRepositoryCustom에 queryBillsBySearchInput 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepositoryCustom.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.oli.HometownPolitician.domain.bill.repository;
 
 import com.oli.HometownPolitician.domain.bill.entity.Bill;
+import com.oli.HometownPolitician.domain.search.input.SearchInput;
 
 import java.util.List;
 
 public interface BillRepositoryCustom {
     List<Bill> queryBillsByIdList(List<Long> idList);
+    List<Bill> queryBillsBySearchInput(SearchInput input);
 }
