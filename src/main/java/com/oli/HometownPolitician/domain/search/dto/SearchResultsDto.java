d827a9881d03da9dc0a2e50b318c5dcb9c83ccbd
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 16:55:49 +0900

    [Add] SearchResultsDto에 from 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/search/dto/SearchResultsDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/search/dto/SearchResultsDto.java
@@ -1,5 +1,7 @@
 package com.oli.HometownPolitician.domain.search.dto;
 
+import com.oli.HometownPolitician.domain.bill.entity.Bill;
+import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
 import com.oli.HometownPolitician.global.argument.output.SlicePaginationDto;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,4 +13,16 @@ import java.util.List;
 public class SearchResultsDto {
     private List<SearchResultDto> list;
     private SlicePaginationDto pagination;
+
+    static public SearchResultsDto from(List<Bill> bills, TargetSlicePaginationInput input) {
+        return SearchResultsDto
+                .builder()
+                .list(
+                        bills.stream()
+                                .map(SearchResultDto::from)
+                                .toList()
+                )
+                .pagination(SlicePaginationDto.from(input, bills.size()))
+                .build();
+    }
 }
