df7f1f87613ba482ccc87b6da3991ceafe06eeaf
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 17:07:03 +0900

    [Add] SearchResultsDto에 fromByPopularityBills 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/search/dto/SearchResultsDto.java
+++ src/main/java/com/oli/HometownPolitician/domain/search/dto/SearchResultsDto.java
@@ -1,6 +1,7 @@
 package com.oli.HometownPolitician.domain.search.dto;
 
 import com.oli.HometownPolitician.domain.bill.entity.Bill;
+import com.oli.HometownPolitician.domain.bill.entity.PopularityBill;
 import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
 import com.oli.HometownPolitician.global.argument.output.SlicePaginationDto;
 import lombok.Builder;
@@ -14,7 +15,7 @@ public class SearchResultsDto {
     private List<SearchResultDto> list;
     private SlicePaginationDto pagination;
 
-    static public SearchResultsDto from(List<Bill> bills, TargetSlicePaginationInput input) {
+    static public SearchResultsDto fromByBills(List<Bill> bills, TargetSlicePaginationInput input) {
         return SearchResultsDto
                 .builder()
                 .list(
@@ -23,6 +24,17 @@ public class SearchResultsDto {
                                 .toList()
                 )
                 .pagination(SlicePaginationDto.from(input, bills.size()))
+                .build();
+    }
+    static public SearchResultsDto fromByPopularityBills(List<PopularityBill> popularityBills, TargetSlicePaginationInput input) {
+        return SearchResultsDto
+                .builder()
+                .list(
+                        popularityBills.stream()
+                                .map(SearchResultDto::from)
+                                .toList()
+                )
+                .pagination(SlicePaginationDto.from(input, popularityBills.size()))
                 .build();
     }
 }
