4b4ae49a7f8610ac78ec4e177f68a3faf5f80609
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 12:06:26 +0900

    [Add] SearchFilterInput 추가

---
+++ src/main/java/com/oli/HometownPolitician/domain/search/input/SearchFilterInput.java
@@ -0,0 +1,15 @@
+package com.oli.HometownPolitician.domain.search.input;
+
+import com.oli.HometownPolitician.domain.committee.input.CommitteeInput;
+import com.oli.HometownPolitician.domain.search.enumeration.SearchResultOrderBy;
+import com.oli.HometownPolitician.domain.tag.dto.TagInput;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class SearchFilterInput {
+    private CommitteeInput committee;
+    private TagInput tag;
+    private SearchResultOrderBy orderBy;
+}
