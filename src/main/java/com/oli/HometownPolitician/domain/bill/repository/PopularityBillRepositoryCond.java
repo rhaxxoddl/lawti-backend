9ae50e80ebbac008b6b3995979e895847a605958
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 15:07:44 +0900

    [Add] PopularityBillRepositoryCond eqId, notDeleted cond 메서드 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/repository/PopularityBillRepositoryCond.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/repository/PopularityBillRepositoryCond.java
@@ -2,11 +2,21 @@ package com.oli.HometownPolitician.domain.bill.repository;
 
 import com.oli.HometownPolitician.global.argument.input.TargetSlicePaginationInput;
 import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.dsl.BooleanExpression;
 
 import static com.oli.HometownPolitician.domain.bill.entity.QPopularityBill.popularityBill;
 
 public class PopularityBillRepositoryCond {
 
+    public BooleanExpression eqId(Long id) {
+        if (id == null)
+            return null;
+        return popularityBill.id.eq(id);
+    }
+
+    public BooleanExpression notDeleted() {
+        return popularityBill.deletedAt.isNull();
+    }
 
     public BooleanBuilder popularityBillDirection(TargetSlicePaginationInput pagination) {
         BooleanBuilder builder = new BooleanBuilder();
