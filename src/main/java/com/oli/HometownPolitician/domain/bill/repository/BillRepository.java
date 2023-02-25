6ba25942792f42e32a7a2f62ab1614425b4b7e6d
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 16:44:02 +0900

    [Add] BillRepository에 PopularityBillRepositoryCustom 연결

--- src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepository.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepository.java
@@ -3,5 +3,5 @@ package com.oli.HometownPolitician.domain.bill.repository;
 import com.oli.HometownPolitician.domain.bill.entity.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BillRepository extends JpaRepository<Bill, Long>, BillRepositoryCustom {
+public interface BillRepository extends JpaRepository<Bill, Long>, BillRepositoryCustom, PopularityBillRepositoryCustom {
 }
