807b89161fe7c520beb1255e24920fb75c8b72bb
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-25 15:04:34 +0900

    [Add] PopularityBillRepositoryCustom 추가

---
+++ src/main/java/com/oli/HometownPolitician/domain/bill/repository/PopularityBillRepositoryCustom.java
@@ -0,0 +1,10 @@
+package com.oli.HometownPolitician.domain.bill.repository;
+
+import com.oli.HometownPolitician.domain.bill.entity.PopularityBill;
+import com.oli.HometownPolitician.domain.search.input.SearchInput;
+
+import java.util.List;
+
+public interface PopularityBillRepositoryCustom {
+    List<PopularityBill> queryPopularityBills(SearchInput input);
+}
