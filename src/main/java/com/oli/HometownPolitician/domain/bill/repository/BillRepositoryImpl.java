7f0107ebed9e6073857e11e20756349f62259d23
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-02-28 11:50:20 +0900

    [Fix] queryBillsBySearchInput 키워드에 따라 검색할 수 있게 join문 수정

--- src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepositoryImpl.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/repository/BillRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.oli.HometownPolitician.domain.bill.repository;
 
 import com.oli.HometownPolitician.domain.bill.entity.Bill;
+import com.oli.HometownPolitician.domain.committee.repository.CommitteeRepositoryCond;
 import com.oli.HometownPolitician.domain.search.input.SearchInput;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.JPAExpressions;
@@ -10,16 +11,22 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static com.oli.HometownPolitician.domain.bill.entity.QBill.bill;
+import static com.oli.HometownPolitician.domain.billTagRelation.entity.QBillTagRelation.billTagRelation;
 import static com.oli.HometownPolitician.domain.billUserRelation.entity.QBillUserRelation.billUserRelation;
 import static com.oli.HometownPolitician.domain.committee.entity.QCommittee.committee;
+import static com.oli.HometownPolitician.domain.politician.entity.QPolitician.politician;
+import static com.oli.HometownPolitician.domain.proposer.entity.QProposer.proposer;
+import static com.oli.HometownPolitician.domain.tag.entity.QTag.tag;
 
 public class BillRepositoryImpl implements BillRepositoryCustom {
     private final JPAQueryFactory queryFactory;
     private final BillRepositoryCond billCond;
+    private final CommitteeRepositoryCond committeeCond;
 
     public BillRepositoryImpl(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
         this.billCond = new BillRepositoryCond();
+        this.committeeCond = new CommitteeRepositoryCond();
     }
 
     @Override
@@ -33,9 +40,11 @@ public class BillRepositoryImpl implements BillRepositoryCustom {
     @Override
     public List<Bill> queryBillsBySearchInput(SearchInput input) {
         return queryFactory
-                .selectFrom(bill)
-                .leftJoin(bill.committee, committee)
-                .leftJoin(bill.followedBillUserRelations, billUserRelation)
+                .select(bill)
+                .from(bill)
+                .leftJoin(bill.committee, committee).fetchJoin()
+                .leftJoin(bill.tags, billTagRelation).leftJoin(billTagRelation.tag, tag)
+                .leftJoin(bill.proposers, proposer).leftJoin(proposer.politician, politician)
                 .where(
                         billCond.billNotDeleted()
                                 .and(billCond.searchFilter(input.getFilter()))
