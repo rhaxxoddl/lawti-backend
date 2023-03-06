6b7bbb623d49639a0c57ca7f7de66a9bcad60793
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-06 18:02:36 +0900

    [Refactor] named builder로 리팩토링

--- src/main/java/com/oli/HometownPolitician/domain/billUserRelation/entity/BillUserRelation.java
+++ src/main/java/com/oli/HometownPolitician/domain/billUserRelation/entity/BillUserRelation.java
@@ -6,17 +6,18 @@ import com.oli.HometownPolitician.domain.user.entity.User;
 import com.oli.HometownPolitician.global.entity.BaseTimeEntity;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 
+@Entity
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Entity
-@Table(name = "bill_user_relations")
+@AllArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
+@Table(name = "bill_user_relations")
 @SequenceGenerator(name = "BILLUSERRELATION_SEQ_GENERATOR", sequenceName = "BILLUSERRELATION_SEQ")
 public class BillUserRelation extends BaseTimeEntity {
 
@@ -30,14 +31,21 @@ public class BillUserRelation extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "bill_id", referencedColumnName = "bill_id", nullable = false)
     private Bill bill;
+    @Builder.Default
     @Column(name = "is_unfollowed", nullable = false)
-    private Boolean isUnfollowed;
+    private Boolean isUnfollowed = false;
     @OneToOne
     @JoinColumn(name = "last_read_bill_message_id", referencedColumnName = "bill_message_id")
     private BillMessage lastReadBillMessage;
 
-    @PrePersist
-    public void prePersist(){
-        this.isUnfollowed = this.isUnfollowed != null && this.isUnfollowed;
+    @Builder(builderClassName = "InitBuilder", builderMethodName = "InitBuilder")
+    public BillUserRelation(User user, Bill bill, Boolean isUnfollowed) {
+        Assert.notNull(user, "user에 null이 들어올 수 없습니다.");
+        Assert.notNull(bill, "bill에 null이 들어올 수 없습니다.");
+
+        this.user = user;
+        this.bill = bill;
+        this.isUnfollowed = isUnfollowed;
+        lastReadBillMessage = null;
     }
 }
