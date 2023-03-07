aede5a58c050946d315df6ac14d4fa419fc1e1f6
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-07 13:48:43 +0900

    [Add] CommitteeDto

---
+++ src/main/java/com/oli/HometownPolitician/domain/committee/dto/CommitteeDto.java
@@ -0,0 +1,28 @@
+package com.oli.HometownPolitician.domain.committee.dto;
+
+import com.oli.HometownPolitician.domain.committee.entity.Committee;
+import io.jsonwebtoken.lang.Assert;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class CommitteeDto {
+    private Long committeeId;
+    private String name;
+
+    @Builder(builderClassName = "InitBuilder", builderMethodName = "InitBuilder")
+    public CommitteeDto(Long committeeId, String name) {
+        Assert.notNull(committeeId, "committeeId에 null이 들어올 수 없습니다");
+        Assert.notNull(name, "name에 null이 들어올 수 없습니다");
+
+        this.committeeId = committeeId;
+        this.name = name;
+    }
+    static public CommitteeDto from(Committee committee) {
+        return InitBuilder()
+                .committeeId(committee.getId())
+                .name(committee.getName())
+                .build();
+    }
+}
