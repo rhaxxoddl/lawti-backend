b8108d14384ae20b20bc807f91aa9f25135438dc
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-07 14:04:14 +0900

    [Add] Committee에 notDeleted cond 추가

--- src/main/java/com/oli/HometownPolitician/domain/committee/repository/CommitteeRepositoryCond.java
+++ src/main/java/com/oli/HometownPolitician/domain/committee/repository/CommitteeRepositoryCond.java
@@ -1,14 +1,11 @@
 package com.oli.HometownPolitician.domain.committee.repository;
 
-import com.oli.HometownPolitician.domain.committee.entity.QCommittee;
-import com.oli.HometownPolitician.domain.committee.input.CommitteeInput;
 import com.querydsl.core.types.dsl.BooleanExpression;
 
-public class CommitteeRepositoryCond {
+import static com.oli.HometownPolitician.domain.committee.entity.QCommittee.committee;
 
-    public BooleanExpression eqCommitteeInput(CommitteeInput input) {
-        if (input == null || input.getCommitteeId() == null)
-            return null;
-        return QCommittee.committee.id.eq(input.getCommitteeId());
+public class CommitteeRepositoryCond {
+    public BooleanExpression notDeleted() {
+        return committee.deletedAt.isNull();
     }
 }
