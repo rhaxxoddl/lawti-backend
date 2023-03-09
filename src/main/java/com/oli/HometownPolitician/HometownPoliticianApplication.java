5cc4b51af2f89d377438fc7ce4fe722ae7269eef
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-10 01:47:27 +0900

    [Add] Application에 @EnableScheduling 추가

--- src/main/java/com/oli/HometownPolitician/HometownPoliticianApplication.java
+++ src/main/java/com/oli/HometownPolitician/HometownPoliticianApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class HometownPoliticianApplication {
 
     public static void main(String[] args) {
