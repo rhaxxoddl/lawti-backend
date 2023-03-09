3ffa151b71cbdafcb69827d7de5d1b90e55f7945
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-09 21:39:01 +0900

    [Refactor] OpenApiProperty 가독성 좋게 수정

--- src/main/java/com/oli/HometownPolitician/global/property/OpenApiProperty.java
+++ src/main/java/com/oli/HometownPolitician/global/property/OpenApiProperty.java
@@ -3,22 +3,25 @@ package com.oli.HometownPolitician.global.property;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @ConstructorBinding
 @RequiredArgsConstructor
-@ConfigurationPropertiesScan
 @ConfigurationProperties(prefix = "open-api")
 public class OpenApiProperty {
-
+    private final Key keys;
     private final Timeout timeouts;
 
-    private final Key keys;
-    public record Timeout(int connect, int response, int read, int write) {
-    }
-
-    public record Key(String publicDataBill, String publicDataPolitician, String openAssembly) {
-    }
+    public record Key(
+            String publicDataBill,
+            String publicDataPolitician,
+            String openAssembly
+    ) {}
+    public record Timeout(
+            int connect,
+            int response,
+            int read,
+            int write
+    ) {}
 }
