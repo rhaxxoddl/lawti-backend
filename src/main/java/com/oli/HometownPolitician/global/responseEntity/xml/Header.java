4d39e0a6e8014d06c7423cb677435dd247780778
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-10 15:08:15 +0900

    [Add] XML 형식에서 공통적으로 사용할 Header response entity 추가

---
+++ src/main/java/com/oli/HometownPolitician/global/responseEntity/xml/Header.java
@@ -0,0 +1,9 @@
+package com.oli.HometownPolitician.global.responseEntity.xml;
+
+import lombok.Data;
+
+@Data
+public class Header {
+    private String resultCode;
+    private String resultMsg;
+}
