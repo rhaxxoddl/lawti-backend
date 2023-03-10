f18a752fb8cf486b5fc8ac0f7fe4e2261a150e4b
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-10 14:43:20 +0900

    [Add] parseCurrentStatusOfCommittees가 잘 동작하는지 확인하는 코드 추가

---
+++ src/test/java/com/oli/HometownPolitician/domain/committee/schedule/CommitteeScheduleTest.java
@@ -0,0 +1,37 @@
+package com.oli.HometownPolitician.domain.committee.schedule;
+
+import com.oli.HometownPolitician.domain.committee.entity.Committee;
+import com.oli.HometownPolitician.domain.committee.repository.CommitteeRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+class CommitteeScheduleTest {
+    @Autowired
+    private CommitteeSchedule committeeSchedule;
+    @Autowired
+    private CommitteeRepository committeeRepository;
+
+    @Test
+    @DisplayName("위원회 현황을 잘 불러와서 저장하는지 확인")
+    void parseCurrentStatusOfCommittees() {
+        List<Committee> beforeCommittees = committeeRepository.qFindAll();
+        Assertions.assertThat(beforeCommittees).isNotNull();
+        Assertions.assertThat(beforeCommittees.size()).isEqualTo(0);
+
+        committeeSchedule.parseCurrentStatusOfCommittees();
+
+        List<Committee> afterCommittees = committeeRepository.qFindAll();
+        Assertions.assertThat(afterCommittees).isNotNull();
+        Assertions.assertThat(afterCommittees.size()).isGreaterThan(10);
+
+    }
+}
