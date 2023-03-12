bd6205e7250dd96a6b24d49fdd2cd4a848c2645f
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-12 15:25:37 +0900

    [Add] ObjectMapperProvider에서 제공하는 ObjectMapper의 설정 조건 추가

--- src/main/java/com/oli/HometownPolitician/global/provider/ObjectMapperProvider.java
+++ src/main/java/com/oli/HometownPolitician/global/provider/ObjectMapperProvider.java
@@ -3,6 +3,7 @@ package com.oli.HometownPolitician.global.provider;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,6 +14,8 @@ public class ObjectMapperProvider {
         return new ObjectMapper()
                 .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES)
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
+                .configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
+                .registerModule(new JavaTimeModule());
     }
 }
