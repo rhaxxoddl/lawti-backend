8b3210bd041f059d822deafb6d92a990ad679271
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-12 22:38:43 +0900

    [Modify] PlenaryResultType애 valeOfLable 추가

--- src/main/java/com/oli/HometownPolitician/domain/bill/enumeration/PlenaryResultType.java
+++ src/main/java/com/oli/HometownPolitician/domain/bill/enumeration/PlenaryResultType.java
@@ -1,21 +1,35 @@
 package com.oli.HometownPolitician.domain.bill.enumeration;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public enum PlenaryResultType {
     PASS("가결"),
     REJECT("부결"),
     ALTERNATIVE_DISCARD("대안반영폐기"),
     DISCARD("폐기"),
     EXPIRED_DISCARD("임기만료폐기"),
+    END_OF_SESSION_DISCARD("회기불계속폐기"),
     WITHDRAWAL("철회"),
     PENDING("계류");
 
-    private final String value;
+    private final String lable;
 
-    PlenaryResultType(String value) {
-        this.value = value;
+    PlenaryResultType(String lable) {
+        this.lable = lable;
     }
 
-    public String getValue() {
-        return value;
+    public String getLable() {
+        return lable;
+    }
+    private static final Map<String, PlenaryResultType> BY_LABEL =
+            Stream.of(values())
+                    .collect(Collectors.toMap(PlenaryResultType::getLable, e -> e));
+    public static PlenaryResultType valueOfLable(String lable) {
+        PlenaryResultType plenaryResultType = BY_LABEL.get(lable);
+        if (plenaryResultType == null)
+            throw new EnumConstantNotPresentException(ProposerKind.class, lable);
+        return plenaryResultType;
     }
 }
