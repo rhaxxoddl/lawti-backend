cc160d411ce6df464fab787f90ccb53bb2e5e786
Sanghwan-Jeon <rhaxxoddl@naver.com>
2023-03-12 15:27:39 +0900

    [Add] WebClientFactory에 설정 조건 추가

--- src/main/java/com/oli/HometownPolitician/global/factory/WebClientFactory.java
+++ src/main/java/com/oli/HometownPolitician/global/factory/WebClientFactory.java
@@ -3,6 +3,7 @@ package com.oli.HometownPolitician.global.factory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 @Component
@@ -15,6 +16,7 @@ public class WebClientFactory {
         return WebClient.builder()
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl("http://apis.data.go.kr/9710000/BillInfoService2")
+                .exchangeStrategies(defaultExchangeStrategies())
                 .build();
     }
 
@@ -24,5 +26,12 @@ public class WebClientFactory {
                 .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .baseUrl("https://open.assembly.go.kr/portal/openapi")
                 .build();
+    }
+    public ExchangeStrategies defaultExchangeStrategies() {
+
+        return ExchangeStrategies.builder().codecs(config -> {
+            config.defaultCodecs().maxInMemorySize(1024 * 1024); // max buffer 1MB 고정. default: 256 * 1024
+            config.defaultCodecs().enableLoggingRequestDetails(true);
+        }).build();
     }
 }
